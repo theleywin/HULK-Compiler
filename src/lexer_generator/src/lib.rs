e3eba95410ea92d6cc94d3a503ced65a143c0031
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2025-06-10 22:33:17 -0400

    feat: parser.lalrpop created and functional, tests OK

--- src/lexer_generator/src/lib.rs
+++ src/lexer_generator/src/lib.rs
@@ -1,3 +1,8 @@
+use lalrpop_util::lalrpop_mod;
+
+lalrpop_mod!(parser);
+
+pub use parser::RegexParser;
 
 pub mod ast;
 
@@ -5,4 +10,5 @@ pub mod ast;
 pub mod test {
     pub mod char_match;
     pub mod test_expression;
+    pub mod test_parsing;
 }
