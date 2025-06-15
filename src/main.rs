940ecb54acb6d8502af9ab1a40ebcf3c009dacb2
Sekai02 <luisalejandroarteagamorales@gmail.com>
2025-06-14 22:01:29 -0400

    mpv for builtin functions, rand still doesn't work, line offset fixed

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,35 @@
-use crate::semantic_analyzer::semantic_analyzer::SemanticAnalyzer;
+use crate::{parser_w_errors::Parser, semantic_analyzer::semantic_analyzer::SemanticAnalyzer};
 use lalrpop_util::lalrpop_mod;
 pub mod ast_nodes;
+pub mod builtin;
+pub mod codegen;
 mod parser_w_errors;
 pub mod semantic_analyzer;
-pub mod codegen;
 mod tokens;
 pub mod types_tree;
 pub mod visitor;
 
 lalrpop_mod!(pub parser);
 
-use crate::parser_w_errors::Parser;
+use crate::builtin::FunctionInjector;
 use codegen::CodeGenerator;
 
 fn main() {
-    let input = r#"
-        for (pene in range(1,5)){
-            let a = "sexo" in {
-                let b = a @ " muy fuerte" in { 
-                    let c = b @ " y muy duro" in {
-                        print(c); 
-                    };
-                };
-            };
-            print( pene );
-        };
-        
-    "#;
+    let raw_input = r#"exp(5);"#;
 
-     let parser = Parser::new();
-    match parser.parse(input) {
+    let function_injector = FunctionInjector::new();
+    let input = function_injector.inject_code(raw_input);
+    let missplacement = function_injector.get_builtin_functions_code_lines() as i32;
+
+    let parser = Parser::new(missplacement);
+    match parser.parse(&input) {
         Ok(mut expr) => {
-            println!("\x1b[32mSyntactic Analyzer OK\x1b[0m");
-            println!("\x1b[0m");
+            println!("Syntactic Analyzer OK");
             let mut semantic_analyzer = SemanticAnalyzer::new();
             let result = semantic_analyzer.analyze(&mut expr);
             match result {
                 Ok(_) => {
-                    println!("\x1b[32mSemantic Analyzer OK\x1b[0m");
+                    println!("Semantic Analyzer OK");
                 }
                 Err(errors) => {
                     println!("\x1b[31mSemantic Errors:");
